8e5b0ce5aca4d6a5e4766bf089c7b3cdad657c8c
nathan-judge <nathanjudge27@gmail.com>
2021-09-17 04:42:50 +0000

    eqArray updated

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,12 +8,20 @@ const assertArraysEqual = function (array1, array2) {
 };
 
 const eqArrays = function (array1, array2) {
-    for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-            return false;
+    if (array1.length > array2.length) {
+        for (let i = 0; i < array1.length; i++) {
+            if (array1[i] !== array2[i]) {
+                return false;
+            }
         }
+    } if (array1.length < array2.length) {
+        for (let i = 0; i < array2.length; i++) {
+            if (array1[i] !== array2[i]) {
+                return false;
+            }
+        }
+
     }
     return true;
 };
-
 
